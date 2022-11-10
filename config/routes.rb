88e7a497a11e67916246cb549b76ec1b19081338
YValdivia07 <yenifervaldivia.hd@gmail.com>
2022-11-10 11:47:52 -0300

    blog_yv_22

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'homepages/index'
   resources :contacts
   resources :homepages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
